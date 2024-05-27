b10cc759c13348141c8f487bebaf89857421c77c
RitiKSIBJr <letter.ritik011@gmail.com>
2024-05-27 09:24:42 +0545

    response page

--- src/web/pages.py
+++ src/web/pages.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
-from src.core.prompts import questions
-from src.core.gemini import main
+from src.core.prompts import questions, get_prompt
+from src.core.gemini import gemini_response
 
 bp = Blueprint("pages", __name__)
 
@@ -9,10 +9,24 @@ bp = Blueprint("pages", __name__)
 def home():
     return render_template("pages/home.html")
 
-@bp.route("/explore")
+@bp.route("/explore", methods=['GET', 'POST'])
 def explore():
     ques = questions()
-    print(ques)
-    # res = main()
-    res = '''**Suitable Hobby Recommendation:** **Indoor Fitness Routine** * Engage in bodyweight exercises such as push-ups, squats, and jumping jacks. * Follow guided workouts from free online platforms like YouTube or apps like Sweat or BetterMe. * Dedicate 15-20 minutes per session to de-stress and improve physical well-being. **Related Hobbies:** * **Yoga or Pilates:** Focus on flexibility, balance, and core strength while improving mindfulness. * **Tai Chi or Qi Gong:** Ancient mind-body practices that promote relaxation, improve circulation, and enhance balance. * **Meditation or Deep Breathing Exercises:** Dedicate a few minutes to practice mindfulness, reduce stress, and calm the mind. * **Indoor Walking or Jogging:** Utilize a treadmill or designated walking area indoors to get some cardio and release endorphins. * **Dancing to Music:** Put on some upbeat music and dance freely to decompress and increase heart rate.'''
-    return render_template("pages/explore.html", question = ques, res=res)
+    return render_template("pages/explore.html", question = ques)
+
+@bp.route("/hobby", methods=['GET', 'POST'])
+def hobby():
+    if request.method == 'POST':
+        ans1 = request.form.get('ques1')
+        ans2 = request.form.get('ques2')
+        ans3 = request.form.get('ques3')
+        ans4 = request.form.get('ques4')
+        ans5 = request.form.get('ques5')
+
+        print(f"Question 1: {ans1}\nQuestion 2: {ans2}\nQuestion 3: {ans3}\nQuestion 4: {ans4}\nQuestion 5: {ans5}\n")
+
+        prompt = get_prompt([ans1, ans2, ans3, ans4, ans5])
+        response = gemini_response(prompt)
+
+    
+    return render_template("pages/hobby.html", prompt=response)
